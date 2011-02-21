8797c43f085c19413178b0e010eab12a9e052d88
sammyrulez <sammyrulez@gmail.com>
2011-02-21 16:03:33 +0100

    initial comit

---
+++ config.ru
@@ -0,0 +1,4 @@
+# This file is used by Rack-based servers to start the application.
+
+require ::File.expand_path('../config/environment',  __FILE__)
+run Trip2to::Application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trip2to::Application.config.secret_token = 'f33b8c726304a52dc31872e645386fa8512ccd9b7ecfcc137fd6f877a32db66deb21c6aae095d5d85ddc4ade348157d6b762f4886323a7f4bf07487428227759'
