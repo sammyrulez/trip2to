33c3c1d6fac6293f2bd7b65f2bfe9749172d4442
Sammyrulez <sammyrulez@gmail.com>
2011-03-02 00:36:32 +0100

    tolto log inutile

--- lib/http_getter_engine.rb
+++ lib/http_getter_engine.rb
@@ -5,7 +5,7 @@ class HttpGetterEngine
 
   def get(url)
       response =  HTTParty.get(url)
-      puts response.body, response.code, response.message, response.headers.inspect
+      #puts response.body, response.code, response.message, response.headers.inspect
       if   response.code != 200
          raise "Response #{response.code}"
       else
