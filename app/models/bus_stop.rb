0261bd1120c4cfee42f529b13c14ae4f9f358fba
sammyrulez <sammyrulez@gmail.com>
2011-03-02 13:58:54 +0100

    add geokit stuff

--- app/models/bus_stop.rb
+++ app/models/bus_stop.rb
@@ -1,26 +1,12 @@
 class BusStop < ActiveRecord::Base
-
-  scope :near, lambda{ |origin_lat, origin_lng ,within|
-      
-                        origin_lat = deg2rad(origin_lat)
-                        origin_lng =  deg2rad(origin_lng)
-                        {
-                        :conditions => %(
-                          (ACOS(COS(#{origin_lat})*COS(#{origin_lng})*COS(RADIANS(bus_stops..stop_lat))*COS(RADIANS(bus_stops..stop_lon))+
-                          COS(#{origin_lat})*SIN(#{origin_lng})*COS(RADIANS(bus_stops.stop_lat))*SIN(RADIANS(bus_stops.stop_lon))+
-                          SIN(#{origin_lat})*SIN(RADIANS(bus_stops.stop_lat)))*3963) <= #{within}
-                        ),
-                        :select => %( bus_stops.*,
-                          (ACOS(COS(#{origin_lat})*COS(#{origin_lng})*COS(RADIANS(bus_stops.stop_lat))*COS(RADIANS(bus_stops.stop_lon))+
-                          COS(#{origin_lat})*SIN(#{origin_lng})*COS(RADIANS(bus_stops.stop_lat))*SIN(RADIANS(bus_stops.stop_lat))+
-                          SIN(#{origin_lat})*SIN(RADIANS(bus_stops.stop_lat)))*3963) AS distance
-                        )
-                      }
-    }
-
-  private
-  def self.deg2rad(deg)
-	  (deg * Math::PI / 180)
+  acts_as_mappable :default_units => :kms , 
+                   :default_formula => :sphere, 
+                   :distance_field_name => :distance,
+                   :lat_column_name => :stop_lat,
+                   :lng_column_name => :stop_lon
+                   
+  def self.find_near(coords, max_distance = 0.1)
+    BusStop.find(:all, :origin =>coords, :within=>max_distance)
   end
 
 end
